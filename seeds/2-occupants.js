84480b190726bc4c8ab69b34f24b6aced2f94769
Keith <kg.anderson@outlook.com>
2021-01-22 10:40:37 +1300

    Updated Seeds

--- seeds/2-occupants.js
+++ seeds/2-occupants.js
@@ -1,13 +1,15 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+        { id: 21, name: 'Kylie Desiree', interests: 'Cooking', allows_pets: true },
+        { id: 22, name: 'Julie Marylu', interests: 'Sports', allows_pets: true },
+        { id: 23, name: 'Shelby Dex', interests: 'Sports', allows_pets: false },
+        { id: 24, name: 'Mojo Jojo', interests: 'Taking over Townsville', allows_pets: true },
+        { id: 25, name: 'Arn Shannon', interests: 'Dancing', allows_pets: false },
+        { id: 26, name: 'Fleur Tatiana', interests: 'Gaming', allows_pets: false },
+        { id: 27, name: 'Leslie Bea', interests: 'Cooking', allows_pets: true },
+        { id: 28, name: 'Clayton Gabe', interests: 'Dancing', allows_pets: false },
+        { id: 29, name: 'Xavier Wilkie', interests: 'Dancing', allows_pets: true },
+        { id: 30, name: 'Royston Rusty', interests: 'Gaming', allows_pets: false },
+          ]);
     });
 };
